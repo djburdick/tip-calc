11e04fdd6ab172c40bb77ba863e0aed2150b89fd
DJ Burdick <burdick.dj@gmail.com>
2013-09-17 21:16:34 -0700

    tip calculator

---
+++ Tip Calculator/Tip Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by DJ Burdick on 9/17/13.
+//  Copyright (c) 2013 DJ Burdick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tip Calculator/Tip Calculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  Tip Calculator
+//
+//  Created by DJ Burdick on 9/17/13.
+//  Copyright (c) 2013 DJ Burdick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
